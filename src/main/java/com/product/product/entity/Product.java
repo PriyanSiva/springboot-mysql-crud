93abfc0888f139ebafc338f9c030c222792968c0
PriyanSiva <priyansiva1995@gmail.com>
2021-01-27 09:10:55 +0530

    Porduct entity

---
+++ src/main/java/com/product/product/entity/Product.java
@@ -0,0 +1,87 @@
+package com.product.product.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	private int quantity;
+	private int price;
+	
+	public Product() {
+		
+	}
+	
+	public Product(String name, int quantity, int price) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price + "]";
+	}
+	
+	
+}
